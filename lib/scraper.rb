19a1e9f7f466ac3f5a8a55646fc4a618cca5c4ee
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-24 12:08:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,15 +18,17 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-#    doc.css(".post").each do |post|
-#      course = Course.new
-#      course.title = post.css("h2").text
-#      course.schedule = post.css(".date").text
-#      course.description = post.css("p").text
   end
 
   def get_courses
     self.get_page.css("post")
+  end
+
+  def make_courses
+      doc.css(".post").each do |post|
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
   end
 end
